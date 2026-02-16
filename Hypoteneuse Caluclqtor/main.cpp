3182e0ecf73c04738e6767b1aac899d2750c4660
Mugiwara960 <danieltogodui59@gmail.com>
2026-02-16 11:48:33 +0000

    Add files via upload

---
+++ Hypoteneuse Caluclqtor/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include<cmath>
+
+using namespace std;
+
+int main(){
+int num1 ;
+int num2;
+
+cout<<"*****HYPOTENEUSE CALCULATOR PROGRAM****"<<endl;
+cout <<"Enter the first number here: " ;
+cin>>num1;
+cout<<"Enter the second number here: " ;
+cin>>num2;
+cout<<"The hypoteneuse is "<<sqrt(pow(num1 , 2) + pow(num2 , 2 ));
+    return 0;
+}
